bdefbfa9d4380af8bcaaacb07b90c411d565323f
Mrunal6708 <mrunaljoshi075@gmail.com>
2025-09-04 00:36:32 +0530

    add fille

---
+++ Q4/Login/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function login(){
+  return(
+    <div>
+      <input type="text" placeholder='username'/>
+      <input type="text" placeholder='password'/>
+      <button>Submit</button>
+    </div>
+  )
+}
+export default login;
